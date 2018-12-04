4ecd1084303026ae3f538077cd4ff5b522038b67
technote <technote.space@gmail.com>
2018-12-05 05:30:37 +0900

    v1.1.1

--- views/admin/progress.php
+++ views/admin/progress.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 1.0.0.0
+ * @version 1.1.1
  * @author technote-space
  * @since 1.0.0.0
  * @copyright technote All Rights Reserved
--- views/admin/script/progress.php
+++ views/admin/script/progress.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 1.0.2.9
+ * @version 1.1.1
  * @author technote-space
  * @since 1.0.0.0
  * @copyright technote All Rights Reserved
