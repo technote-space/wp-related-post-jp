f5c967e0a8503543a52249b79b06acd2fba9e585
technote-space <technote.space@gmail.com>
2018-12-09 06:34:51 +0900

    v1.1.3

--- src/classes/controllers/api/admin/index/result.php
+++ src/classes/controllers/api/admin/index/result.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * @version 1.0.3.2
+ * @version 1.1.3
  * @author technote-space
  * @since 1.0.1.9
+ * @since 1.1.3
  * @copyright technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
  * @link https://technote.space
