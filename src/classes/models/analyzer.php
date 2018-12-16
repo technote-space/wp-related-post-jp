6f1f3b90c7637b9fdb48287dd701536c142720cb
technote <technote.space@gmail.com>
2018-12-16 23:39:01 +0900

    v1.2.1

--- src/classes/models/analyzer.php
+++ src/classes/models/analyzer.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * @version 1.1.3
+ * @version 1.2.1
  * @author technote-space
  * @since 1.0.0.0
  * @since 1.1.3
+ * @since 1.2.1 Changed: timing to limit sentence length
  * @copyright technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
  * @link https://technote.space
