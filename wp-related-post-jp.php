d360f5474b173e7c0589ce0045e7898c11bcbad3
Technote <technote.space@gmail.com>
2019-10-08 01:17:33 +0900

    v1.3.21

--- wp-related-post-jp.php
+++ wp-related-post-jp.php
@@ -1,14 +1,14 @@
 <?php
-/*
-Plugin Name: WP Related Post JP
-Plugin URI: https://github.com/technote-space/wp-related-post-jp
-Description: WP Related Post JP provides functions to get related posts.
-Author: Technote
-Version: 1.3.20
-Author URI: https://technote.space
-Text Domain: wrpj
-Domain Path: /languages/
-*/
+/**
+ * Plugin Name: WP Related Post JP
+ * Plugin URI: https://github.com/technote-space/wp-related-post-jp
+ * Description: WP Related Post JP provides functions to get related posts.
+ * Author: Technote
+ * Version: 1.3.21
+ * Author URI: https://technote.space
+ * Text Domain: wrpj
+ * Domain Path: /languages/
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
