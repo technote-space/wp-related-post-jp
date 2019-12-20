f733c9d368f4744651d64a2469cf7489781d0dcf
Technote <technote.space@gmail.com>
2019-12-21 00:59:12 +0900

    v1.3.22

--- wp-related-post-jp.php
+++ wp-related-post-jp.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/technote-space/wp-related-post-jp
  * Description: WP Related Post JP provides functions to get related posts.
  * Author: Technote
- * Version: 1.3.21
+ * Version: 1.3.22
  * Author URI: https://technote.space
  * Text Domain: wrpj
  * Domain Path: /languages/
