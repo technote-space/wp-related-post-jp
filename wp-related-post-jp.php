4465aa056c03300e94fc9f5b846e86c3adcf609c
Technote <technote.space@gmail.com>
2021-02-28 02:30:56 +0900

    chore: update wp version

--- wp-related-post-jp.php
+++ wp-related-post-jp.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/technote-space/wp-related-post-jp
  * Description: WP Related Post JP provides functions to get related posts.
  * Author: Technote
- * Version: 1.4.19
+ * Version: 1.4.20
  * Author URI: https://technote.space
  * Text Domain: wrpj
  * Domain Path: /languages/
